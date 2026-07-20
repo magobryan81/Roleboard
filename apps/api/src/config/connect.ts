78907fd201fb1b5a2db506deb9c8a94f1018a3e2
magobryan81 <magobryan81@gmail.com>
2026-07-20 11:25:25 +0800

    fix DNS SRV

--- apps/api/src/config/connect.ts
+++ apps/api/src/config/connect.ts
@@ -2,7 +2,7 @@ import { Db, MongoClient, ServerApiVersion } from "mongodb";
 import path from "path";
 import dotenv from "dotenv";
 
-dotenv.config({ path: path.resolve(__dirname, "../../../.env") });
+dotenv.config({ path: path.resolve(__dirname, "../../.env") });
 
 // Create a MongoClient with a MongoClientOptions object to set the Stable API version
 const client = new MongoClient(process.env.ATLAS_URI as string, {
--- apps/api/src/server.ts
+++ apps/api/src/server.ts
@@ -1,12 +1,17 @@
+import { setServers } from 'node:dns/promises';
 import { connectToServer } from "./config/connect";
 import express from "express";
 import cors from "cors";
-
+setServers(['1.1.1.1', '8.8.8.8']); 
 const app = express();
 const PORT = 3000;
 
 app.use(cors());
 app.use(express.json());
+
+app.get("/", (req, res) => {
+    res.send('Hello, World!');
+})
 
 app.listen(PORT, async () => {
     await connectToServer();
