5692282485f40d1937899d732a4441b30d5fc955
magobryan81 <magobryan81@gmail.com>
2026-08-24 22:54:39 +0800

    add session routes

--- apps/api/src/routes/session.routes.ts
+++ apps/api/src/routes/session.routes.ts
@@ -1,5 +1,6 @@
 import express from "express";
+import * as SessionController from "../controllers/session.controller";
 
-const router = Router();
+const router = express.Router();
 
-router.get("/", SessionController.getSessions)
+router.get("/", SessionController.getSessionsHandler)
