0cff0d5d772fbb1e3f77785a07d277a2f751a25c
magobryan81 <magobryan81@gmail.com>
2026-09-09 10:23:00 +0800

    msg

--- apps/api/src/routes/job.routes.ts
+++ apps/api/src/routes/job.routes.ts
@@ -10,6 +10,6 @@ router.patch("/:jobId", JobController.updateJobHandler)
 router.patch("/:jobId/archived", JobController.archiveJobHandler);
 router.patch("/:jobId/unarchived", JobController.unarchiveJobHandler);
 router.patch("/:jobId/interview/stage", JobController.updateJobInterviewStageHandler);
-router.post("/:jobId/interview/note", JobController.updateJobInterviewNoteHandler);
+router.post("/:jobId/interview/note", JobController.updateJobInterviewasdsadNoteHandler);
 
 export default router;
